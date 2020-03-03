5bd355d3fe114753db6a2a065f869bea03d4b98f
rcdegaldo <rcdegaldo@idealista.com>
2020-03-03 09:32:28 +0100

    fizzbuzz_plusplus first iteration

--- fizzbuzzplusplus.py
+++ fizzbuzzplusplus.py
@@ -1,13 +1,27 @@
+from math import fmod
+
 nums = [2,3,5]
 words = ["fizz", "buzz", "bazz"]
 
 def fizzbuzz_plusplus(nums, words):
   # TODO: complete
   nums_least_common_multiple = 1
-  for i in range(len(nums)):
-      nums_least_common_multiple = nums_least_common_multiple * nums[i]
-      print(nums_least_common_multiple)
   
+  for i in range(len(nums)):
+    nums_least_common_multiple = nums_least_common_multiple * nums[i]
+  print(nums_least_common_multiple)
+  
+  for i in range(0,nums_least_common_multiple+1):
+    
+    fizzbuzzed_item = ""
+
+    for j in range(0,len(nums)):
+      if fmod(i,nums[j]) == 0:
+        fizzbuzzed_item = fizzbuzzed_item + words[j]
+    if fizzbuzzed_item == "":
+      fizzbuzzed_item = i
+    print(fizzbuzzed_item)
+
   return []
 
 fizzbuzz_plusplus(nums, words)
